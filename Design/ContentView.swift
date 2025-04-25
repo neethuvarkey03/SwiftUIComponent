1f1b249a5583189aab257bc420df7ceaec16fee5
Neethu <varkey.neethu@gmail.com>
2025-04-25 18:56:42 +0530

    Background colour change

--- Design/ContentView.swift
+++ Design/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
             })
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.secondary)
+        .background(Color.red)
     }
 
 }
